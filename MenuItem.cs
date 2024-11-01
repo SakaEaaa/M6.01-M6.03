902236ce2b86c5a83f062f1fe250647a8a044ed6
SakaEaaa <146702440+SakaEaaa@users.noreply.github.com>
2024-11-01 23:15:00 +0100

    all done

---
+++ MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+public class MenuItem : ITableEntity
+{
+    public string PartitionKey { get; set; } = "uge1"; // Standardværdi for uge
+    public string RowKey { get; set; }                 // Dag som nøgle (Mandag, Tirsdag osv.)
+    public string KoldRet { get; set; }
+    public string VarmRet { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Azure.Data.Tables;
+using System.Collections.Generic;
+using System.Linq;
+using Azure;
 
-namespace IBAS_kantine.Pages;
-
-public class IndexModel : PageModel
+namespace IBAS_kantine.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-    public void OnGet()
-    {
+        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
 
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            // Hent forbindelse til Azure Table Storage
+            string connectionString = _configuration.GetConnectionString("AzureTableStorage");
+            TableClient tableClient = new TableClient(connectionString, "IBASmenu123");
+
+
+            // Hent menupunkterne fra tabellen
+            Pageable<MenuItem> menuItems = tableClient.Query<MenuItem>();
+
+
+            // Tilføj til liste for at blive vist i HTML
+            MenuItems = menuItems.ToList();
+
+            // Log hentningen
+            _logger.LogInformation("Kantinens menu blev hentet fra Azure Table Storage.");
+        }
     }
 }
