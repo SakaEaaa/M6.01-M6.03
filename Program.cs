2e618e056aeb4a828581856a4b55c70eb94b044e
SakaEaaa <146702440+SakaEaaa@users.noreply.github.com>
2024-11-06 12:52:13 +0100

    a3

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Azure.Data.Tables;
+using Azure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Tilføj TableServiceClient til DI-containeren med brug af URI og Managed Identity
+string tableStorageUri = "https://ibasbikeproduction123.table.core.windows.net"; // Udskift med din storage account URI
+builder.Services.AddSingleton(new TableServiceClient(new Uri(tableStorageUri), new DefaultAzureCredential()));
 
 var app = builder.Build();
 
