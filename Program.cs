ba6c55c61732b83deca2aaf41bb893b64e7d92f0
SakaEaaa <146702440+SakaEaaa@users.noreply.github.com>
2024-11-12 09:43:21 +0100

    envoirment varible (virker ikke)

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-// Tilføj TableServiceClient til DI-containeren med brug af URI og Managed Identity
-string tableStorageUri = "https://ibasbikeproduction123.table.core.windows.net"; // Udskift med din storage account URI
-builder.Services.AddSingleton(new TableServiceClient(new Uri(tableStorageUri), new DefaultAzureCredential()));
+// Get Table Storage URI from appsettings.json
+string tableStorageUri = builder.Configuration["Storage:TableEndpoint"];
+
+if (!string.IsNullOrEmpty(tableStorageUri))
+{
+    builder.Services.AddSingleton(new TableServiceClient(new Uri(tableStorageUri), new DefaultAzureCredential()));
+}
+else
+{
+    throw new InvalidOperationException("Table Storage URI is not configured. Please set the Storage:TableEndpoint in appsettings.json.");
+}
 
 var app = builder.Build();
 
@@ -16,17 +24,12 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
